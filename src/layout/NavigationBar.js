6b6b230a2fdaedc24911f7aa6947b2b6e758a558
soohyn <sooheyonjo@gmail.com>
2024-12-20 01:01:57 +0900

    [Feat] navItem navigationBar 적용

--- src/layout/NavigationBar.js
+++ src/layout/NavigationBar.js
@@ -1,19 +1,25 @@
 import { html, LitElement, css } from "lit";
 import resetCSS from "./resetCSS";
+import "@/components/atoms/NavItem.js";
 
 class NavigationBar extends LitElement {
   static get styles() {
     return [
       resetCSS,
       css`
+        nav {
+          display: flex;
+          flex-direction: column;
+          position: fixed;
+          bottom: 0;
+          width: 100vw;
+        }
+
         ul {
           display: flex;
           flex-direction: row;
-          justify-content: space-around;
-        }
-
-        li {
-          padding: 1rem;
+          padding: 0 0.75rem 0.5rem 0.75rem;
+          justify-content: space-around; //!
         }
       `,
     ];
@@ -23,10 +29,10 @@ class NavigationBar extends LitElement {
     return html`
       <nav>
         <ul>
-          <li><a href="/src/pages/artEventPage/">전시</a></li>
-          <li><a href="/src/pages/placePage/">장소</a></li>
-          <li><a href="/src/pages/reviewPage/">리뷰</a></li>
-          <li><a href="/src/pages/myPage/">저장</a></li>
+          <c-nav-item label="전시" pageName="artEventPage" icon="/src/assets/icons/exhibition.svg"></c-nav-item>
+          <c-nav-item label="장소" pageName="placePage" icon="/src/assets/icons/place.svg"></c-nav-item>
+          <c-nav-item label="리뷰" pageName="reviewPage" icon="/src/assets/icons/review.svg"></c-nav-item>
+          <c-nav-item label="MY" pageName="myPage" icon="/src/assets/icons/my.svg"></c-nav-item>
         </ul>
       </nav>
     `;
