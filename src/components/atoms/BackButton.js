4edb61f996c73d219c702dd763733deb31e7331b
juhyuny <juhyun5358@gmail.com>
2024-12-21 19:23:28 +0900

    [Feat] BackButton.js 컴포넌트 분리

---
+++ src/components/atoms/BackButton.js
@@ -0,0 +1,45 @@
+import { html, css } from "lit";
+import IndexCSS from "../../styles/indexCSS";
+
+class BackButton extends IndexCSS {
+  static get styles() {
+    return [
+      ...IndexCSS.styles,
+      css/*css */ `
+        .back {
+          width: 1.5rem;
+          height: 1.5rem;
+        }
+      `,
+    ];
+  }
+
+  render() {
+    return html/* html */ ` <button class="back" type="button" aria-label="뒤로가기">
+      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <g clip-path="url(#clip0_2949_6558)">
+          <g filter="url(#filter0_d_2949_6558)">
+            <path d="M17.5098 3.8701L15.7298 2.1001L5.83984 12.0001L15.7398 21.9001L17.5098 20.1301L9.37984 12.0001L17.5098 3.8701Z" fill="#1E1E1E" />
+          </g>
+        </g>
+        <defs>
+          <filter id="filter0_d_2949_6558" x="-10.1602" y="-5.8999" width="43.6699" height="51.8" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+            <feOffset dy="8" />
+            <feGaussianBlur stdDeviation="8" />
+            <feComposite in2="hardAlpha" operator="out" />
+            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0" />
+            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2949_6558" />
+            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2949_6558" result="shape" />
+          </filter>
+          <clipPath id="clip0_2949_6558">
+            <rect width="24" height="24" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
+    </button>`;
+  }
+}
+
+customElements.define("back-button", BackButton);
--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,6 +1,8 @@
 import { html, css } from "lit";
 import IndexCSS from "../styles/indexCSS";
 
+import "../components/atoms/BackButton";
+
 class Header extends IndexCSS {
   static properties = {
     pageName: { attribute: true },
@@ -20,6 +22,8 @@ class Header extends IndexCSS {
   }
 
   static get styles() {
+    
+    console.log(...IndexCSS.styles);
     return [
       ...IndexCSS.styles,
       css/* css */ `
@@ -33,11 +37,8 @@ class Header extends IndexCSS {
           position: relative;
         }
 
-        header .back {
-          width: 1.5rem;
-          height: 1.5rem;
-          border: none;
-          padding: 0;
+        back-button {
+          width: 100%;
           position: absolute;
           left: var(--spacing-6); /* 16px */
           top: 50%;
@@ -71,39 +72,12 @@ class Header extends IndexCSS {
 
   renderBackButton(pageName) {
     if (pageName) {
-      return html`<button class="back" type="button" aria-label="뒤로가기">
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-          <g clip-path="url(#clip0_2949_6558)">
-            <g filter="url(#filter0_d_2949_6558)">
-              <path d="M17.5098 3.8701L15.7298 2.1001L5.83984 12.0001L15.7398 21.9001L17.5098 20.1301L9.37984 12.0001L17.5098 3.8701Z" fill="#1E1E1E" />
-            </g>
-          </g>
-          <defs>
-            <filter id="filter0_d_2949_6558" x="-10.1602" y="-5.8999" width="43.6699" height="51.8" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-              <feFlood flood-opacity="0" result="BackgroundImageFix" />
-              <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
-              <feOffset dy="8" />
-              <feGaussianBlur stdDeviation="8" />
-              <feComposite in2="hardAlpha" operator="out" />
-              <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0" />
-              <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2949_6558" />
-              <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2949_6558" result="shape" />
-            </filter>
-            <clipPath id="clip0_2949_6558">
-              <rect width="24" height="24" fill="white" />
-            </clipPath>
-          </defs>
-        </svg>
-      </button>`;
+      return html` <back-button></back-button> `;
     }
   }
 
   render() {
-    return html/* html */ `
-    <header>
-      ${this.renderHeaderContent(this.pageName)} 
-      ${this.renderBackButton(this.pageName)}
-      </header> `;
+    return html/* html */ ` <header>${this.renderHeaderContent(this.pageName)} ${this.renderBackButton(this.pageName)}</header> `;
   }
 }
 
