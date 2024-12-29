e13753a74242ee783078c76a3be6f17286695d3e
juhyuny <juhyun5358@gmail.com>
2024-12-29 15:30:11 +0900

    [Modify] 링크태그 추가 및 스타일 수정, 임시 마크업에 aria 속성 추가 - a 태그 사용 - 스타일 수정 - aria 속성 추가

--- src/components/organisms/ExhibitCard.js
+++ src/components/organisms/ExhibitCard.js
@@ -37,14 +37,10 @@ class ExhibitCard extends IndexCSS {
             gap: var(--spacing-3);
           }
 
-          .exhibit-title {
-            line-height: 1.5;
-          }
-
           .keyword-wrapper {
             background-color: var(--gray-50);
             border-radius: var(--radius-2xs);
-            padding: 0.375rem 0.5rem;
+            padding: 0.125rem 0.5rem;
 
             .keyword {
               display: inline;
@@ -60,7 +56,7 @@ class ExhibitCard extends IndexCSS {
             background-color: var(--green);
           }
 
-          .review_num {
+          .review-num {
             color: var(--gray-500);
           }
 
@@ -77,37 +73,39 @@ class ExhibitCard extends IndexCSS {
   render() {
     return html/* html */ `
       <article class="exhibit-card">
-        <figure>
-          <div class="img-wrapper">
-            <img style="height: 100%" src="src/assets/profile/juhyun_park.jpg" alt="전시 이름" />
-          </div>
-          <figcaption class="exhibit-info">
-            <strong class="exhibit-title label-md">소행성이 빛을 내는 방법</strong>
-
-            <div class="keyword-wrapper paragraph-xs">
-              <span class="emoji">🗓️</span>
-              <p class="keyword">
-                <time datetime="2024-12-01">2024.12.1</time> ~
-                <time datetime="2024-12-31">2024.12.24</time>
-              </p>
+        <a href="/pages/ExhibitDetailPage/">
+          <figure>
+            <div class="img-wrapper">
+              <img style="height: 100%" src="/src/assets/profile/juhyun_park.jpg" alt="전시 이름" />
             </div>
+            <figcaption class="exhibit-info">
+              <strong class="exhibit-title label-md">소행성이 빛을 내는 방법</strong>
 
-            <div class="keyword-wrapper paragraph-xs">
-              <span class="emoji">📍</span>
-              <p class="keyword">갤러리헬렌에이</p>
-            </div>
+              <div class="keyword-wrapper paragraph-xs">
+                <span class="emoji">🗓️</span>
+                <p class="keyword">
+                  <time datetime="2024-12-01">2024.12.1</time> ~
+                  <time datetime="2024-12-24">2024.12.24</time>
+                </p>
+              </div>
 
-            <div class="keyword-wrapper paragraph-xs">
-              <span class="emoji">👥</span>
-              <p class="keyword">모든 연령 관람 가능</p>
-            </div>
+              <div class="keyword-wrapper paragraph-xs">
+                <span class="emoji">📍</span>
+                <p class="keyword">갤러리헬렌에이</p>
+              </div>
 
-            <div class="badge label-xs">전시중</div>
+              <div class="keyword-wrapper paragraph-xs">
+                <span class="emoji">👥</span>
+                <p class="keyword">모든 연령 관람 가능</p>
+              </div>
 
-            <p class="review_num paragraph-xs">방문자 리뷰 33</p>
-          </figcaption>
-        </figure>
-        <button class="bookmark" type="button" aria-label="북마크">
+              <div class="badge label-xs">전시중</div>
+
+              <p class="review-num paragraph-xs">방문자 리뷰 33</p>
+            </figcaption>
+          </figure>
+        </a>
+        <button class="bookmark" type="button" aria-label="북마크 추가" aria-pressed="false">
           <img src="/src/assets/icons/bookmark.svg" alt="" aria-hidden="true" />
         </button>
       </article>
